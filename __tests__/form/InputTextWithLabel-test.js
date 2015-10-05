b71d20a004fb3b46bf88a5b43af73f7032ba94ba
Tam Sayers <tamsayers@hotmail.com>
2015-10-05 15:41:04 +0100

    added more tests

--- __tests__/form/InputTextWithLabel-test.js
+++ __tests__/form/InputTextWithLabel-test.js
@@ -1,27 +1,45 @@
-jest.dontMock("../../build/lib/form/InputTextWithLabel.js");
+jest.dontMock('../../build/lib/form/InputTextWithLabel.js');
 
-describe("InputText", function() {
-  var $, React, DOM, TestUtils;
+describe('InputText', function() {
+  var $, DOM, TestUtils, inputValue;
 
   beforeEach(function() {
     $ = require('jquery');
-    React = require("react/addons");
-    TestUtils = React.addons.TestUtils; 
+    var React = require('react/addons');
+    TestUtils = React.addons.TestUtils;
+    inputValue = undefined;
 
-    var InputTextWithLabel = require("../../build/lib/form/InputTextWithLabel.js");
-    DOM = TestUtils.renderIntoDocument(
-      <InputTextWithLabel block="bem-block" name="inputName">Label Text</InputTextWithLabel>
+    var InputTextWithLabel = require('../../build/lib/form/InputTextWithLabel.js');
+    var component = TestUtils.renderIntoDocument(
+      <InputTextWithLabel block='bem-block' name='inputName' value={inputValue}>Label Text</InputTextWithLabel>
     );
-  });
-  
-  it("renders a div with the correctly generated class", function() {
-    var block = TestUtils.findRenderedDOMComponentWithClass(DOM, "bem-block__inputName");
-  });
-  
-  it("contains a label for component name", function() {
-    var label = TestUtils.findRenderedDOMComponentWithClass(DOM, "bem-block__inputName");
     
-    expect(React.findDOMNode(DOM).children[0].tagName).toEqual("LABEL");
-    expect(React.findDOMNode(DOM).children[0].getAttribute('for')).toEqual('inputName');
+    DOM = React.findDOMNode(TestUtils.findRenderedDOMComponentWithTag(component, 'div'));
   });
+  
+  it('renders a compnent with the correctly generated class', function() {
+    expect($(DOM).attr('class')).toEqual('bem-block__inputName');
+  });
+  
+  it('contains a label for component name', function() {
+    expect($(DOM).find('label').attr('for')).toEqual('inputName');
+  });
+  
+  it('the label contains the given text', function() {
+    expect($(DOM).find('label').html()).toEqual('Label Text');
+  });
+  
+  it('the input is of type text', function() {
+    expect($(DOM).find('input').attr('type')).toEqual('text');
+  });
+  
+  it('the input name is the component name', function() {
+    expect($(DOM).find('input').attr('name')).toEqual('inputName');
+  });
+  
+//  it('the input value is the component value', function() {
+//    expect($(DOM).find('input').attr('value')).toEqual(undefined);
+//    inputValue = 'val';
+//    expect($(DOM).find('input').attr('value')).toEqual('val');
+//  });
 });
