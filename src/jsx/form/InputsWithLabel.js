2d8df542199b6d00e5de59b93ae31a8a58f71e26
Tam Sayers <tamsayers@hotmail.com>
2015-10-22 19:45:49 +0100

    started styling form

--- src/jsx/form/InputsWithLabel.js
+++ src/jsx/form/InputsWithLabel.js
@@ -2,11 +2,9 @@ var React = require('react');
 
 var Input = React.createClass({
   render() {
-    var camelCaseToHyphenated = str => str.replace(/(.)([A-Z])/, '$1-$2').toLowerCase();
-    var { type, name, children, block, value, onChange, other } = this.props;
-    var className = block + "__" + camelCaseToHyphenated(name);
+    var { type, name, children, value, onChange, other } = this.props;
     return (
-      <div className={className}>
+      <div className='input-with-label'>
         <label htmlFor={name}>{children}</label>
         <input type={type} name={name} value={this.props.value} onChange={this.props.onChange} {...other}/>
       </div>
--- src/jsx/query/QueryForm.js
+++ src/jsx/query/QueryForm.js
@@ -5,8 +5,10 @@ module.exports = React.createClass({
   render() {
     return (
       <form name='query-form' onSubmit={this.props.onSubmit}>
-        <Input.Text block="query-form" name="from" value={this.props.query.from} onChange={this.props.onChange}>Date From</Input.Text>
-        <Input.Text block="query-form" name="till" value={this.props.query.till} onChange={this.props.onChange}>Date Till</Input.Text>
+        <fieldset>
+          <Input.Text name="from" value={this.props.query.from} onChange={this.props.onChange}>Date From</Input.Text>
+          <Input.Text name="till" value={this.props.query.till} onChange={this.props.onChange}>Date Till</Input.Text>
+        </fieldset>
         <button className="query-form__submit" type="submit">Submit</button>
       </form>
     );
