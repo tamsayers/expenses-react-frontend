2eaca64abb39e1ebdc4c346f88f120455375a4ba
Tam Sayers <tamsayers@hotmail.com>
2015-11-02 07:20:07 +0000

    removing block param

--- src/jsx/add/ExpenseInput.js
+++ src/jsx/add/ExpenseInput.js
@@ -10,12 +10,12 @@ module.exports = React.createClass({
 
     return (
       <fieldset className='expense'>
-        <Inputs.Text block='expense' name='date' value={expense.date} onChange={onChange}>Date</Inputs.Text>
-        <Inputs.Text block='expense' name='description' value={expense.description} onChange={onChange}>Description</Inputs.Text>
-        <Inputs.Text block='expense' name='clientName' value={expense.clientName} onChange={onChange}>Client Name</Inputs.Text>
-        <Inputs.Text block='expense' name='supplier' value={expense.supplier} onChange={onChange}>Supplier</Inputs.Text>
-        <Inputs.Number block='expense' name='cost.amount' value={expense.cost.amount} onChange={onChange} step='0.01'>Cost</Inputs.Number>
-        <SelectOptions block='expense' name='cost.type' value={expense.cost.type} options={options} onChange={onChange}/>
+        <Inputs.Text name='date' value={expense.date} onChange={onChange}>Date</Inputs.Text>
+        <Inputs.Text name='description' value={expense.description} onChange={onChange}>Description</Inputs.Text>
+        <Inputs.Text name='clientName' value={expense.clientName} onChange={onChange}>Client Name</Inputs.Text>
+        <Inputs.Text name='supplier' value={expense.supplier} onChange={onChange}>Supplier</Inputs.Text>
+        <Inputs.Number name='cost.amount' value={expense.cost.amount} onChange={onChange} step='0.01'>Cost</Inputs.Number>
+        <SelectOptions name='cost.type' value={expense.cost.type} options={options} onChange={onChange}/>
       </fieldset>
     );
   }
