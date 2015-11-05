0e1a196e2f9966957a7d93b7a033d6212dd06a12
Tam Sayers <tamsayers@Tams-MacBook-Pro.local>
2015-11-05 15:42:06 +0000

    updated front end to match api changes

--- src/jsx/query/Result.js
+++ src/jsx/query/Result.js
@@ -13,14 +13,20 @@ module.exports = React.createClass({
           <div className='header-item'>Description</div>
           <div className='header-item'>Client Name</div>
           <div className='header-item'>Supplier</div>
-          <div className='header-item'>Cost</div>
+          <div className='header-item'>Gross</div>
+          <div className='header-item'>Vat</div>
+          <div className='header-item'>Net</div>
+          <div className='header-item'>Details</div>
         </div>
         <div className={this.props.className + '-data'}>
           <div className='data-item'>{this.props.data.date}</div>
           <div className='data-item'>{this.props.data.description}</div>
           <div className='data-item'>{this.props.data.clientName}</div>
           <div className='data-item'>{this.props.data.supplier}</div>
-          <div className='data-item'>£{this.props.data.cost.amount.toFixed(2)} ({this.props.data.cost.costType})</div>
+          <div className='data-item'>£{this.props.data.amount.gross}</div>
+          <div className='data-item'>{this.props.data.amount.vat}</div>
+          <div className='data-item'>{this.props.data.amount.net}</div>
+          <div className='data-item'>{this.props.data.amount.details}</div>
         </div>
       </li>
     );
